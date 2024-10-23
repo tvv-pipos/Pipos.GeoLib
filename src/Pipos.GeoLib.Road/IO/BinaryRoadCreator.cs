9e00b888428d6c258a561324fa79ec9551174faf
Mikael Nilsson <mikaelnilsson80@outlook.com>
2024-10-23 08:49:10 +0000

    BinaryRoadCreator

--- src/Pipos.GeoLib.Road/IO/BinaryRoadCreator.cs
+++ src/Pipos.GeoLib.Road/IO/BinaryRoadCreator.cs
@@ -6,28 +6,20 @@ using NetTopologySuite.Geometries;
 using Pipos.GeoLib.Core.Model;
 
 namespace Pipos.GeoLib.Road.IO;
-public class GeoPackageImport
+public class BinaryRoadCreator
 {
     private List<Edge> Edges = new List<Edge>();
-    private Dictionary<UInt64, Node> Nodes = new Dictionary<ulong, Node>();
-    //private List<Segment> Segments = new List<Segment>();
     private int SegmentCount = 0;
 
-    public GeoPackageImport()
-    {
-
-    }
-
-    public GeoPackageImport Clear()
+    public BinaryRoadCreator Clear()
     {
         Edges.Clear();
-        Nodes.Clear();
         SegmentCount = 0;
 
         return this;
     }
 
-    public GeoPackageImport ImportGeoPackage(string filePath)
+    public BinaryRoadCreator ImportGeoPackage(string filePath)
     {
         string connectionString = $"Data Source={filePath};Version=3;";
 
@@ -54,141 +46,7 @@ public class GeoPackageImport
         return this;
     }
 
-
-    private void ReadTable(string tableName, SQLiteConnection connection)
-    {
-        uint nodeCount = 0;
-        uint edgeCount = 0;
-        YearSet years = new YearSet(); 
-        Clear();
-
-        var geoReader = new GeoPackageGeoReader();
-        string selectNetowrk = $"SELECT geom, b_hogst_225, f_hogst_225, klass_181, extlen, F_ForbjudenFardriktning, B_ForbjudenFardriktning, Farjeled, typ_41 FROM {tableName};";
-        using (var command = new SQLiteCommand(selectNetowrk, connection))
-        {
-            using (var reader = command.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    byte[] wkb = (byte[])reader["geom"];
-                    Geometry geometry = geoReader.Read(wkb);
-
-                    Coordinate[] coordinates;
-
-                    if (geometry is LineString lineString)
-                    {
-                        coordinates = lineString.Coordinates;
-                        if (coordinates.Length < 2)
-                            continue;
-                    }
-                    else
-                    {
-                        continue;
-                    }
-
-                    byte b_hogst_225 = ReadByte(reader, "b_hogst_225");
-                    byte f_hogst_225 = ReadByte(reader, "f_hogst_225");
-                    byte klass_181 = ReadByte(reader, "klass_181");
-                    float extlen = Convert.ToSingle(reader["extlen"]);
-                    bool F_ForbjudenFardriktning = Convert.ToBoolean(reader["F_ForbjudenFardriktning"]);
-                    bool B_ForbjudenFardriktning = Convert.ToBoolean(reader["B_ForbjudenFardriktning"]);
-                    bool Farjeled = Convert.ToBoolean(reader["Farjeled"]);
-                    byte typ_41 = ReadByte(reader, "typ_41");
-
-                    Attribute attribute = new Attribute
-                    {
-                        Class = klass_181,
-                        Ferry = Farjeled,
-                        ForwardProhibited = F_ForbjudenFardriktning,
-                        BackwardProhibited = B_ForbjudenFardriktning,
-                        Motorway = typ_41 == 1
-                    };
-
-                    float prevX = (float)coordinates[0].X;
-                    float prevY = (float)coordinates[0].Y;
-                    float lastX = (float)coordinates[coordinates.Length - 1].X;
-                    float lastY = (float)coordinates[coordinates.Length - 1].Y;
-
-                    UInt64 source_pos = (UInt64)MathF.Round(prevX) | ((UInt64)MathF.Round(prevY) << 32);
-                    UInt64 target_pos = (UInt64)MathF.Round(lastX) | ((UInt64)MathF.Round(lastY) << 32);
-
-                    Node? sourceNode;
-                    Node? targetNode;
-                    bool nodesExists = true;
-                    bool edgeExists = false;
-
-                    if (!Nodes.TryGetValue(source_pos, out sourceNode))
-                    {
-                        nodesExists = false;
-                        sourceNode = new Node(nodeCount++);
-                        Nodes.Add(source_pos, sourceNode);
-                    }
-
-                    if (!Nodes.TryGetValue(target_pos, out targetNode))
-                    {
-                        nodesExists = false;
-                        targetNode = new Node(nodeCount++);
-                        Nodes.Add(target_pos, targetNode);
-                    }
-
-                    List<float> segments = new List<float>((coordinates.Length - 1) * 4);
-
-                    for (int i = 1; i < coordinates.Length; i++)
-                    {
-                        float x = (float)coordinates[i].X;
-                        float y = (float)coordinates[i].Y;
-                        if (x == prevX && y == prevY)
-                        {
-                            continue;
-                        }
-
-                        int index = (i - 1) * 4;
-                        segments.Add(prevX);
-                        segments.Add(prevY);
-                        segments.Add(x);
-                        segments.Add(y);
-  
-                        prevX = x;
-                        prevY = y;
-                    }
-
-                    Edge edge = new Edge(edgeCount, sourceNode, targetNode, extlen, f_hogst_225, b_hogst_225, segments.ToArray(), attribute, years);
-
-                    if (nodesExists)
-                    {
-                        foreach (Edge e in sourceNode.Edges)
-                        {
-                            if (e.IsSame(edge))
-                            {
-                                edgeExists = true;
-                            }
-                        }
-                    }
-
-                    if (edgeExists)
-                    {
-                        continue;
-                    }
-
-                    if (edge.Segments.Length == 0)
-                        continue;
-
-                    edgeCount++;
-                    SegmentCount += edge.Segments.Length;
-                    sourceNode.Edges.Add(edge);
-                    targetNode.Edges.Add(edge);
-                    Edges.Add(edge);
-
-                    /*for(uint s = 0; s < edge.Segments.Length; s++)
-                    {
-                        Segments.Add(edge.Segments[s]);
-                    }*/
-                }
-            }
-        }
-    }
-
-    public GeoPackageImport ExportToBinaryFile(string path)
+    public BinaryRoadCreator ExportToBinaryFile(string path)
     {
         UInt32 headerNumber = 0x9596;
         using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write))
@@ -216,7 +74,7 @@ public class GeoPackageImport
         return this;     
     }
 
-    public GeoPackageImport SetFerrySpeed(byte speed)
+    public BinaryRoadCreator SetFerrySpeed(byte speed)
     {
         foreach (Edge edge in Edges)
         {
@@ -232,7 +90,24 @@ public class GeoPackageImport
         return this;
     }
 
-    public GeoPackageImport RemoveSmallerIslands()
+    public BinaryRoadCreator Validate()
+    {
+        foreach (Edge edge in Edges)
+        {
+            if(edge.ForwardSpeed == 0 && !edge.Attribute.ForwardProhibited)
+            {
+                throw new Exception("edge.ForwardSpeed == 0 && !edge.Attribute.ForwardProhibited");
+            }
+
+            if(edge.BackwardSpeed == 0 && !edge.Attribute.BackwardProhibited)
+            {
+                throw new Exception("edge.BackwardSpeed == 0 && !edge.Attribute.BackwardProhibited");
+            }
+        }
+        return this;
+    }
+
+    public BinaryRoadCreator RemoveSmallerIslands()
     {
         Queue<Edge> q = new Queue<Edge>();
         bool[] visited = new bool[Edges.Count];
@@ -300,13 +175,21 @@ public class GeoPackageImport
         return this;
     }
 
-    public GeoPackageImport Optimize()
+    public BinaryRoadCreator Optimize()
     {
+        HashSet<Node> nodes = new HashSet<Node>();
+
+        Edges.ForEach(e => 
+        {
+            nodes.Add(e.Source);
+            nodes.Add(e.Target);
+        });
+
         int count = 1;
         while (count > 0)
         {
             count = 0;
-            foreach (var (id, node) in Nodes)
+            foreach (var node in nodes)
             {
                 if (node.Edges.Count == 2)
                 {
@@ -364,10 +247,8 @@ public class GeoPackageImport
                 }
             }
         }
-        Nodes = Nodes.Where(n => n.Value.Id != UInt32.MaxValue).ToDictionary();
         Edges = Edges.Where(e => e.Id != UInt32.MaxValue).ToList();
 
-        // Recalculate ids
         for (int i = 0; i < Edges.Count; i++)
         {
             Edges[i].Id = (uint)i;
@@ -375,11 +256,138 @@ public class GeoPackageImport
 
         return this;
     }
-
-
-    
+  
     private byte ReadByte(SQLiteDataReader reader, string columnName)
     {
         return reader[columnName] != DBNull.Value ? Convert.ToByte(reader[columnName]) : default(byte);
+    }
+
+    private void ReadTable(string tableName, SQLiteConnection connection)
+    {
+        Dictionary<UInt64, Node> nodes = new Dictionary<ulong, Node>();
+        uint nodeCount = 0;
+        uint edgeCount = 0;
+        YearSet years = new YearSet(); 
+        Clear();
+
+        var geoReader = new GeoPackageGeoReader();
+        string selectNetowrk = $"SELECT geom, b_hogst_225, f_hogst_225, klass_181, extlen, F_ForbjudenFardriktning, B_ForbjudenFardriktning, Farjeled, typ_41 FROM {tableName};";
+        using (var command = new SQLiteCommand(selectNetowrk, connection))
+        {
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    byte[] wkb = (byte[])reader["geom"];
+                    Geometry geometry = geoReader.Read(wkb);
+
+                    Coordinate[] coordinates;
+
+                    if (geometry is LineString lineString)
+                    {
+                        coordinates = lineString.Coordinates;
+                        if (coordinates.Length < 2)
+                            continue;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                    byte b_hogst_225 = ReadByte(reader, "b_hogst_225");
+                    byte f_hogst_225 = ReadByte(reader, "f_hogst_225");
+                    byte klass_181 = ReadByte(reader, "klass_181");
+                    float extlen = Convert.ToSingle(reader["extlen"]);
+                    bool F_ForbjudenFardriktning = Convert.ToBoolean(reader["F_ForbjudenFardriktning"]);
+                    bool B_ForbjudenFardriktning = Convert.ToBoolean(reader["B_ForbjudenFardriktning"]);
+                    bool Farjeled = Convert.ToBoolean(reader["Farjeled"]);
+                    byte typ_41 = ReadByte(reader, "typ_41");
+
+                    Attribute attribute = new Attribute
+                    {
+                        Class = klass_181,
+                        Ferry = Farjeled,
+                        ForwardProhibited = F_ForbjudenFardriktning,
+                        BackwardProhibited = B_ForbjudenFardriktning,
+                        Motorway = typ_41 == 1
+                    };
+
+                    float prevX = (float)coordinates[0].X;
+                    float prevY = (float)coordinates[0].Y;
+                    float lastX = (float)coordinates[coordinates.Length - 1].X;
+                    float lastY = (float)coordinates[coordinates.Length - 1].Y;
+
+                    UInt64 source_pos = (UInt64)MathF.Round(prevX) | ((UInt64)MathF.Round(prevY) << 32);
+                    UInt64 target_pos = (UInt64)MathF.Round(lastX) | ((UInt64)MathF.Round(lastY) << 32);
+
+                    Node? sourceNode;
+                    Node? targetNode;
+                    bool nodesExists = true;
+                    bool edgeExists = false;
+
+                    if (!nodes.TryGetValue(source_pos, out sourceNode))
+                    {
+                        nodesExists = false;
+                        sourceNode = new Node(nodeCount++);
+                        nodes.Add(source_pos, sourceNode);
+                    }
+
+                    if (!nodes.TryGetValue(target_pos, out targetNode))
+                    {
+                        nodesExists = false;
+                        targetNode = new Node(nodeCount++);
+                        nodes.Add(target_pos, targetNode);
+                    }
+
+                    List<float> segments = new List<float>((coordinates.Length - 1) * 4);
+
+                    for (int i = 1; i < coordinates.Length; i++)
+                    {
+                        float x = (float)coordinates[i].X;
+                        float y = (float)coordinates[i].Y;
+                        if (x == prevX && y == prevY)
+                        {
+                            continue;
+                        }
+
+                        int index = (i - 1) * 4;
+                        segments.Add(prevX);
+                        segments.Add(prevY);
+                        segments.Add(x);
+                        segments.Add(y);
+  
+                        prevX = x;
+                        prevY = y;
+                    }
+
+                    Edge edge = new Edge(edgeCount, sourceNode, targetNode, extlen, f_hogst_225, b_hogst_225, segments.ToArray(), attribute, years);
+
+                    if (nodesExists)
+                    {
+                        foreach (Edge e in sourceNode.Edges)
+                        {
+                            if (e.IsSame(edge))
+                            {
+                                edgeExists = true;
+                            }
+                        }
+                    }
+
+                    if (edgeExists)
+                    {
+                        continue;
+                    }
+
+                    if (edge.Segments.Length == 0)
+                        continue;
+
+                    edgeCount++;
+                    SegmentCount += edge.Segments.Length;
+                    sourceNode.Edges.Add(edge);
+                    targetNode.Edges.Add(edge);
+                    Edges.Add(edge);
+                }
+            }
+        }
     }
 }
